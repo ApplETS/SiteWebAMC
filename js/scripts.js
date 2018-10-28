a7ca8d797d74b42a2d7499c676438faf303b8e15
Gracia <gracia.dufort-jeanty.1@ens.etsmtl.ca>
2018-10-28 14:27:37 -0400

    resolve conflict

--- js/scripts.js
+++ js/scripts.js
@@ -32,9 +32,7 @@
     });
 
     //compte a rebour
-    var clock;
-
-    clock = $('.clock').FlipClock({
+    var clock = $('.clock').FlipClock({
         clockFace: 'DailyCounter',
         autoStart: false,
         callbacks: {
@@ -45,10 +43,33 @@
             }
         }
     });
+    var dateFin = new Date('2018-02-20 00:00:00');
+    var dateNow = new Date();
 
-    clock.setTime(220880);
+    clock.setTime(dateDiff(dateNow, dateFin));
     clock.setCountdown(true);
     clock.start();
     /****/
 
 })(jQuery);
+
+function dateDiff(date1, date2){
+    var diff = {}                           // Initialisation du retour
+    var tmp = date2 - date1;
+
+    tmp = Math.floor(tmp/1000);             // Nombre de secondes entre les 2 dates
+    diff.sec = tmp % 60;                    // Extraction du nombre de secondes
+
+    tmp = Math.floor((tmp-diff.sec)/60);    // Nombre de minutes (partie entière)
+    diff.min = tmp % 60;                    // Extraction du nombre de minutes
+
+    tmp = Math.floor((tmp-diff.min)/60);    // Nombre d'heures (entières)
+    diff.hour = tmp % 24;                   // Extraction du nombre d'heures
+
+    tmp = Math.floor((tmp-diff.hour)/24);   // Nombre de jours restants
+    diff.day = tmp;
+
+    totalSec = diff.sec + (diff.min * 60) + (diff.hour * 60 * 60) + (diff.day * 24 * 60 * 60);
+
+    return totalSec;
+}
