e5c5d4d60a559fc330051cc19080655fece93a50
Wei Chen <jowei@outlook.com>
2019-01-15 00:22:34 -0500

    Language skeleton

--- js/scripts.js
+++ js/scripts.js
@@ -31,6 +31,55 @@
        $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
     });
 	
+    //let langCode = $("html").attr("lang");
+    let langCode = "FR";
+    console.log(langCode + " loading...");
+    $.getJSON("./lang/"+ langCode +".json", function (result) {
+        console.log("result", result);
+        $("meta[name=description]").attr("content", result.meta_description);
+       
+        $("title").text(result.html_title);
+        $("h1.title_short").text(result.title_short);
+        $("h1.title_long").text(result.title_long);
+        $("h2.title_clock").text(result.title_clock);
+
+        $("span.nav_toggle").text(result.nav_toggle);
+
+        $("ul.nav a.what_is_amc").text(result.menu.what_is_amc);
+        $("ul.nav a.objective").text(result.menu.objective);
+        $("ul.nav a.previously").text(result.menu.previously);
+        $("ul.nav a.schedule").text(result.menu.schedule);
+        $("ul.nav a.faq").text(result.menu.faq);
+        $("ul.nav a.sponsors").text(result.menu.sponsors);
+
+        $("h4.date_address").text(result.date_address);
+
+        $("span.browser_not_supported").text(result.browser_not_supported);
+
+        $("h1.what_is_amc").text(result.menu.what_is_amc);
+        $("p.intro_amc").text(result.intro_amc);
+        $("a.more_modal").text(result.more_modal_link);
+
+        $("h1.objective").text(result.menu.objective);
+
+        $("h3.title_competition").text(result.subcategory.competition.title);
+        $("p.text_competition").text(result.subcategory.competition.text);
+        $("h3.title_fun").text(result.subcategory.fun.title);
+        $("p.text_fun").text(result.subcategory.fun.text);
+        $("h3.title_teamwork").text(result.subcategory.teamwork.title);
+        $("p.text_teamwork").text(result.subcategory.teamwork.text);
+
+        $("h1.previously").text(result.menu.previously);
+        $("p.more_images").text(result.more_images);
+
+        $("h1.schedule").text(result.menu.schedule);
+        $("p.schedule_text").text(result.schedule_text);
+
+        $("h1.coming_soon").text(result.coming_soon);
+    });
+    console.log("Finish loading" + langCode)
+	
+	
     //compte a rebour
     var clock = $('.clock').FlipClock({
         clockFace: 'DailyCounter',
